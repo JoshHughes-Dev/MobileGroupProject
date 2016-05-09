101d689438a37e7cef5e5f08e612c05a03038525
saphfira <alex@eduroam-student-206-111.lut.ac.uk>
2016-05-09 19:05:32 +0100

    Revert "Revert "Event Database Dump Prep""
    This reverts commit 6a5043d11d7e4a1c542c28dbe8407b5040ccc876.

---
+++ StudentOrganiser/app/src/main/java/com/mobilegroupproject/studentorganiser/data/EventsData.java
@@ -0,0 +1,16 @@
+package com.mobilegroupproject.studentorganiser.data;
+
+import com.mobilegroupproject.studentorganiser.activities.ApiTestActivity;
+
+import java.util.List;
+
+/**
+ * Created by alex on 09/05/2016.
+ */
+public class EventsData {
+    ApiTestActivity apiTestActivity = new ApiTestActivity();
+
+    public List<List<String>> getEventsData {
+
+    }
+}
