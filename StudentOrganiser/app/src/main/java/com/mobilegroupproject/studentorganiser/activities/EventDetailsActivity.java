8caf9dea86bd8055dce8c44c7694c2ce9e1fe8a2
Daniel Cohen <analoguecolour@gmail.com>
2016-05-12 19:15:45 +0100

    added share icon

--- StudentOrganiser/app/src/main/java/com/mobilegroupproject/studentorganiser/activities/EventDetailsActivity.java
+++ StudentOrganiser/app/src/main/java/com/mobilegroupproject/studentorganiser/activities/EventDetailsActivity.java
@@ -1,10 +1,17 @@
 package com.mobilegroupproject.studentorganiser.activities;
 
 import android.content.res.Configuration;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.os.Bundle;
 import android.widget.Toast;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
 import com.mobilegroupproject.studentorganiser.R;
 import com.mobilegroupproject.studentorganiser.fragments.EventDetailsFragment;
 import com.mobilegroupproject.studentorganiser.model.ExtendedWeekViewEvent;
@@ -12,6 +19,7 @@ import com.mobilegroupproject.studentorganiser.model.ExtendedWeekViewEvent;
 public class EventDetailsActivity extends AppCompatActivity implements EventDetailsFragment.OnFragmentInteractionListener {
 
     private final static String EVENT_DETAILS_FRAGMENT_TAG = "eventDetailsFragment";
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,7 @@ public class EventDetailsActivity extends AppCompatActivity implements EventDeta
 
 
         // Check if the device is in landscape mode
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ) {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
 
             //Check is device is 'large' layout
             if ((getResources().getConfiguration().screenLayout &
@@ -51,7 +59,7 @@ public class EventDetailsActivity extends AppCompatActivity implements EventDeta
     }
 
     //TODO write code that updates event data
-    public void onEventDetailsUpdate(ExtendedWeekViewEvent selectedEvent){
+    public void onEventDetailsUpdate(ExtendedWeekViewEvent selectedEvent) {
 
         Toast.makeText(getApplicationContext(), "todo update", Toast.LENGTH_SHORT).show();
 
@@ -62,9 +70,33 @@ public class EventDetailsActivity extends AppCompatActivity implements EventDeta
                 getSupportFragmentManager().findFragmentByTag(EVENT_DETAILS_FRAGMENT_TAG);
 
         //update UI and current local event data
-        if(eventDetailsFragment != null){
+        if (eventDetailsFragment != null) {
             eventDetailsFragment.getArguments().putParcelable(EventDetailsFragment.SELECTED_EVENT_DATA, selectedEvent);
             eventDetailsFragment.updateEventDetailsUI();
+        }
+    }
+
+    // BEGIN_INCLUDE(get_sap)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu resource
+        getMenuInflater().inflate(R.menu.eventdetailsactivity, menu);
+
+        // Retrieve the share menu item
+        MenuItem shareItem = menu.findItem(R.id.menu_share);
+
+        // Now get the ShareActionProvider from the item
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        // Get the ViewPager's current item position and set its ShareIntent.
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
         }
     }
 }
